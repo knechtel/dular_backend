2f9db7e4ada3e27392527fe690248d1dbd264f37
maiquelknechtel <maiquelknechtel@gmail.com>
2020-12-13 17:00:23 -0300

    Adicionando usuario DTO.

---
+++ src/main/java/com/eletronica/dular/DTO/UsuarioDTO.java
@@ -0,0 +1,34 @@
+package com.eletronica.dular.DTO;
+
+/**
+ * Created by maiquelknechtel on 12/13/20.
+ */
+public class UsuarioDTO {
+    private Integer id;
+    private String nome;
+    private String senha;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
