4350ab30b170f6ab8182144919b0c38e1e333594
maiquelknechtel <maiquelknechtel@gmail.com>
2020-12-13 16:39:02 -0300

    Configurando database.

--- src/main/java/com/eletronica/dular/conf/DataSourceConfig.java
+++ src/main/java/com/eletronica/dular/conf/DataSourceConfig.java
@@ -16,7 +16,7 @@ public class DataSourceConfig {
     public DataSource datasource() {
         return DataSourceBuilder.create()
                 .driverClassName("org.mariadb.jdbc.Driver")
-                .url("jdbc:mariadb://localhost:3306/qintess")
+                .url("jdbc:mariadb://localhost:3306/dular")
                 .username("root")
                 .password("123")
                 .build();
