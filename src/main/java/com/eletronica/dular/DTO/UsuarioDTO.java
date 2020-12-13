8238c494ea454ba7fc694f46e497c67e59a48c23
maiquelknechtel <maiquelknechtel@gmail.com>
2020-12-13 17:11:48 -0300

    Conversão de usuario p/ usuarioDTO.

--- src/main/java/com/eletronica/dular/DTO/UsuarioDTO.java
+++ src/main/java/com/eletronica/dular/DTO/UsuarioDTO.java
@@ -1,5 +1,7 @@
 package com.eletronica.dular.DTO;
 
+import com.eletronica.dular.domain.Usuario;
+
 /**
  * Created by maiquelknechtel on 12/13/20.
  */
@@ -7,6 +9,13 @@ public class UsuarioDTO {
     private Integer id;
     private String nome;
     private String senha;
+
+    public UsuarioDTO(Integer id, String nome, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+    }
+    public UsuarioDTO(){}
 
     public Integer getId() {
         return id;
@@ -30,5 +39,8 @@ public class UsuarioDTO {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+    public UsuarioDTO build(Usuario usuario){
+        return new UsuarioDTO(usuario.getId(),usuario.getNome(),usuario.getSenha());
     }
 }
--- src/main/java/com/eletronica/dular/controller/UsuarioController.java
+++ src/main/java/com/eletronica/dular/controller/UsuarioController.java
@@ -1,7 +1,26 @@
 package com.eletronica.dular.controller;
 
+import com.eletronica.dular.DTO.UsuarioDTO;
+import com.eletronica.dular.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by maiquelknechtel on 12/13/20.
  */
+@RestController
+@RequestMapping("/api/usuario")
 public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @RequestMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    public UsuarioDTO create(@RequestBody UsuarioDTO usuarioDTO){
+        return null;
+
+    }
 }
