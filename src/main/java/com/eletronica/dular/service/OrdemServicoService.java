6b73512fc066bede8e3bc15362e534787038fad4
maiquelknechtel <maiquelknechtel@gmail.com>
2020-12-15 16:29:32 -0300

    Adicionando método service para ordem de serviço.

---
+++ src/main/java/com/eletronica/dular/service/OrdemServicoService.java
@@ -0,0 +1,22 @@
+package com.eletronica.dular.service;
+
+import com.eletronica.dular.DAO.OrdemServicoDAO;
+import com.eletronica.dular.domain.OrdemServico;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by maiquelknechtel on 12/15/20.
+ */
+public class OrdemServicoService {
+    @Autowired
+    private OrdemServicoDAO ordemServicoDAO;
+
+    public OrdemServico create(OrdemServico ordemServico) {
+        return ordemServicoDAO.save(ordemServico);
+    }
+    public List<OrdemServico> findAll(){
+        return (List<OrdemServico>) ordemServicoDAO.findAll();
+    }
+}
