294986d043979e6b8716c85fb50f7f9e57e53918
maiquelknechtel <maiquelknechtel@gmail.com>
2020-12-15 16:34:57 -0300

    Adicionando a notação Service.

--- src/main/java/com/eletronica/dular/service/OrdemServicoService.java
+++ src/main/java/com/eletronica/dular/service/OrdemServicoService.java
@@ -3,12 +3,14 @@ package com.eletronica.dular.service;
 import com.eletronica.dular.DAO.OrdemServicoDAO;
 import com.eletronica.dular.domain.OrdemServico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by maiquelknechtel on 12/15/20.
  */
+@Service
 public class OrdemServicoService {
     @Autowired
     private OrdemServicoDAO ordemServicoDAO;
